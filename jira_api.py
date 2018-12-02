d4a40eec5cfedc197e26e71992329f5fe37a0e01
Snoop <jonmedwig@gmail.com>
2018-12-01 22:07:34 -0500

    Add stub for add_all_worklogs method

--- jira_api.py
+++ jira_api.py
@@ -5,11 +5,6 @@ from jira import JIRA
 # credentials are stored in ~/.netrc
 jira = JIRA({'server': 'https://skywatch.atlassian.net'})
 
-issue = 'OO-762'
-time_spent = '1.5m'
-comment = 'comment_test_test'
-started = datetime(2006, 11, 21, 16, 30)
-
 
 def get_worklogs(issue):
     issue = jira.issue(issue)
@@ -36,5 +31,26 @@ def add_worklog(issue, time_spent, comment, started):
     return wl
 
 
+def worklog_exists_in_jira(worklog):
+    pass
+
+
+def add_all_worklogs(worklogs):
+    """Adds all worklogs to JIRA, excluding those that already exist"""
+    # For each worklog
+    for local_worklog in worklogs:
+        # Check if wl exits in JIRA
+        if worklog_exists_in_jira(local_worklog):
+            # If yes, log that and skip
+            print('worklog exists in JIRA!')
+            continue
+        # If no, add_wl and log that
+        print('stubbed add_worklog cmd')
+
+
 if __name__ == '__main__':
-    get_worklogs(issue)
+    issue = 'OO-762'
+    time_spent = '1.5m'
+    comment = 'comment_test_test'
+    started = datetime(2006, 11, 21, 16, 30)
+    print(get_worklogs(issue))
