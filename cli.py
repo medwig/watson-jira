bbcd048fdb8857d6d9906db64773a8de0d0c1713
Snoop <jonmedwig@gmail.com>
2019-03-08 19:39:48 -0500

    Add sync command to cli

--- cli.py
+++ cli.py
@@ -1,4 +1,6 @@
 import json
+from datetime import datetime
+from dateutil.parser import parse
 
 import click
 import simplejson
@@ -9,15 +11,48 @@ from src import jira
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
 
+def get_logs(date, jira_only=False, tempo_format=False):
+    logs = watson.report_day(date, jira_only)
+    if tempo_format:
+        logs = watson.report_to_worklogs(logs)
+    return logs
+
+
+def to_ymd(datestring):
+    return datestring.split('T')[0]
+
+
 @click.group(context_settings=CONTEXT_SETTINGS)
 @click.version_option(version='1.0.0')
 def greet():
     pass
 
+
+@greet.command()
+@click.option('--date', default='None', help='date to sync logs')
+@click.option('--issue', default=None, help='only sync logs for this issue')
+def sync(**kwargs):
+    date = kwargs['date']
+    issue = kwargs['issue']
+    logs = get_logs(date, jira_only=True, tempo_format=True)
+    if issue:
+        logs = [l for l in logs if l['issue'] == issue] 
+
+    for log in logs:
+        worklogs = jira.get_worklogs(log['issue'])
+        if any([log['comment'] == wl['comment'] for wl in worklogs]):
+            # Log already exists in Jira worklogs
+            print('Log already exists')
+        else:
+            # Log does not exist in Jira, upload
+            print('syncing log')
+            jira.add_worklog(**log)
+        print('-'*20)
+
+
 @greet.command()
 @click.option('--issue', default=None, help='get worklogs from this issue')
 @click.option('--id', default=None, help='get specific worklog by id')
-# @click.option('--tempo-format', is_flag=True, help='format logs for tempo timesheet')
 def tempo(**kwargs):
     issue = kwargs['issue']
     _id = kwargs['id']
@@ -36,11 +71,9 @@ def tempo(**kwargs):
 def logs(**kwargs):
     date = kwargs['date']
     jira_only = kwargs['jira_only']
-    logs = watson.report_day(date, jira_only)
+    tempo_format = kwargs['tempo_format']
 
-    if kwargs['tempo_format']:
-        logs = watson.report_to_worklogs(logs)
-
+    logs = get_logs(date, jira_only, tempo_format)
     print(json.dumps(logs))
 
 if __name__ == '__main__':
