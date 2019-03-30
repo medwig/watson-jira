ced9267d50fe69ece8817cf409ae2f8b761cfa84
Jonathan Medwig <jonmedwig@gmail.com>
2019-03-29 21:56:23 -0400

    Add test for all basic cli commands

--- watson_jira/tests/test_cli.py
+++ watson_jira/tests/test_cli.py
@@ -12,12 +12,24 @@ def runner():
 
 
 def test_logs(runner):
+    result = runner.invoke(cli.main, ["logs"])
+    assert result.exit_code == 0
+    assert isinstance(json.loads(result.output), list)
+
+
+def test_logs_jiraonly(runner):
+    result = runner.invoke(cli.main, ["logs", "--jira-only"])
+    assert result.exit_code == 0
+    assert isinstance(json.loads(result.output), list)
+
+
+def test_logs_tempoformat(runner):
     result = runner.invoke(cli.main, ["logs", "--jira-only", "--tempo-format"])
     assert result.exit_code == 0
     assert isinstance(json.loads(result.output), list)
 
 
-def test_tempo(runner):
+def test_tempo_issue(runner):
     result = runner.invoke(cli.main, ["tempo", "--issue", "AP-217"])
     assert result.exit_code == 0
     assert isinstance(json.loads(result.output), list)
