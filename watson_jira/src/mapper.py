97368be65937c5f26ab0f49835c1f2c3057241ce
Jonathan Medwig <jonmedwig@gmail.com>
2023-02-02 15:47:47 -0500

    Make issue_specified_in_tag default behaviour

--- watson_jira/src/mapper.py
+++ watson_jira/src/mapper.py
@@ -43,8 +43,8 @@ def map(project, tags, is_interactive):
                 jira_issue = process_single_issue(mapping)
             elif mapping["type"] == "issue_per_project":
                 jira_issue = process_issue_per_project(mapping, project)
-            elif mapping["type"] == "issue_specified_in_tag":
-                jira_issue = process_issue_specified_in_tag(tags)
+        else: # default to issue_specified_in_tag
+            jira_issue = process_issue_specified_in_tag(tags)
 
     # backward compatibility - resolve jira issue from project name
     if jira_issue is None and is_jira_issue(project):
