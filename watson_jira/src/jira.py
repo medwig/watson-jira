8a88971ad8210301fa822787db396ad853a2f169
Jonathan Medwig <jonmedwig@gmail.com>
2023-02-06 16:19:33 -0500

    Refactor jira conn (#16)
    * remove globals from jira methods
    * remove jira_connect from cli

--- watson_jira/src/jira.py
+++ watson_jira/src/jira.py
@@ -3,100 +3,85 @@ from watson_jira.src import config
 from jira import JIRA
 from typing import Optional
 
-jira: Optional[JIRA] = None
-
 
 class JiraException(Exception):
     pass
 
 
-def invalidate():
-    global jira
-    jira = None
-
-
 def connect():
-    global jira
-    if jira is None:
-        jiraconfig = config.jira()
-        try:
-            if jiraconfig['pat']:
-                auth_method = 'pat'
-                headers = JIRA.DEFAULT_OPTIONS['headers'].copy()
-                headers['Authorization'] = jiraconfig['pat']
-                jira = JIRA(
-                    server=jiraconfig['server'], options={'headers': headers}
-                )
+    jiraconfig = config.jira()
+    try:
+        if jiraconfig['pat']:
+            auth_method = 'pat'
+            headers = JIRA.DEFAULT_OPTIONS['headers'].copy()
+            headers['Authorization'] = jiraconfig['pat']
+            jira = JIRA(
+                server=jiraconfig['server'], options={'headers': headers}
+            )
 
-            elif jiraconfig['apiToken']:
-                auth_method = 'apiToken'
-                auth = (jiraconfig['email'], jiraconfig['apiToken'])
-                jira = JIRA(server=jiraconfig['server'], basic_auth=auth)
+        elif jiraconfig['apiToken']:
+            auth_method = 'apiToken'
+            auth = (jiraconfig['email'], jiraconfig['apiToken'])
+            jira = JIRA(server=jiraconfig['server'], basic_auth=auth)
 
-            else:
-                auth_method = 'cookie'
-                headers = JIRA.DEFAULT_OPTIONS['headers'].copy()
-                headers['cookie'] = jiraconfig['cookie']
-                jira = JIRA(
-                    server=jiraconfig['server'], options={'headers': headers}
-                )
+        else:
+            auth_method = 'cookie'
+            headers = JIRA.DEFAULT_OPTIONS['headers'].copy()
+            headers['cookie'] = jiraconfig['cookie']
+            jira = JIRA(
+                server=jiraconfig['server'], options={'headers': headers}
+            )
 
-            return (jiraconfig['server'], auth_method)
-        except Exception:
-            raise JiraException('Connection failed')
+        return jira
+    except Exception:
+        raise JiraException('Connection failed')
 
 
 def get_worklog(issue, _id):
-    global jira
-    if jira is None:
-        raise JiraException('Jira not connected')
-    worklog = jira.worklog(issue, _id)
-    return vars(worklog)
+    jira_conn = connect()
+    worklog = jira_conn.worklog(issue, _id)
+    wl = {
+        'issue': issue,
+        'comment': getattr(worklog, 'comment', None),
+        'started': worklog.started,
+        'timeSpent': worklog.timeSpent,
+        'id': worklog.id,
+    }
+    return wl
 
 
 def delete_worklog(issue, id):
-    global jira
-    if jira is None:
-        raise JiraException('Jira not connected')
-    worklog = jira.worklog(issue, id)
+    jira_conn = connect()
+    worklog = jira_conn.worklog(issue, id)
     worklog.delete()
 
 
 def get_worklogs(issue):
-    global jira
-    if jira is None:
-        raise JiraException('Jira not connected')
-    worklogs = jira.worklogs(issue)
-    parsed_worklogs = []
-    for worklog in worklogs:
-        wl = {
+    jira_conn = connect()
+    worklogs = jira_conn.worklogs(issue)
+    parsed_worklogs = [
+        {
             'issue': issue,
-            'comment': worklog.comment
-            if hasattr(worklog, 'comment')
-            else None,
+            'comment': getattr(worklog, 'comment', None),
             'started': worklog.started,
             'timeSpent': worklog.timeSpent,
             'id': worklog.id,
-        }
-        parsed_worklogs.append(wl)
+        } for worklog in worklogs
+    ]
     return parsed_worklogs
 
 
 def add_worklog(issue, timeSpent, comment, started):
-    global jira
-    if jira is None:
-        raise JiraException('Jira not connected')
-    wl = jira.add_worklog(
+    jira_conn = connect()
+    wl = jira_conn.add_worklog(
         issue, timeSpent=timeSpent, comment=comment, started=parse(started)
     )
     return wl
 
 
 def test_conn():
-    global jira
-    if jira is None:
-        raise JiraException('Jira not connected')
-    user = jira.current_user('displayName')
+    jira_conn = connect()
+    user = jira_conn.current_user('displayName')
     try:
         assert user != 'anonymous'
     except AssertionError:
